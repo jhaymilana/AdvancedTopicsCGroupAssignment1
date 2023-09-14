9f402071e786f4cc7bdef6f6a21036babcf88074
UDAY-2997 <udaythakur.shely@gmail.com>
2023-09-14 11:57:38 -0500

    Created Business Model and linkning tables PersonBusiness and BuisnessAddress

---
+++ AdvancedTopicsCGroupAssignment1/Models/Business.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AdvancedTopicsCGroupAssignment1.Models
+{
+    public class Business
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(3, ErrorMessage = "Name must have at least 3 characters.")]
+        [MaxLength(100, ErrorMessage = "Name has a limit of 100 characters.")]
+        public string Name { get; set; }
+
+    
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email Address")]
+        public string EmailAddress { get; set; }
+
+        public HashSet<PersonBusiness> personBusinesses { get; set; }
+        public HashSet<BusinessAddress> businesssAdresses { get; set; }
+
+        public Business(string name, string emailAdress, string phoneNumber) 
+        {
+            Name = name;
+            EmailAddress = emailAdress;
+            PhoneNumber = phoneNumber;
+            personBusinesses = new HashSet<PersonBusiness>();
+            businesssAdresses = new HashSet<BusinessAddress>();
+        }
+    }
+}
---
+++ AdvancedTopicsCGroupAssignment1/Models/BusinessAddress.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AdvancedTopicsCGroupAssignment1.Models
+{
+    public class BusinessAddress
+    {
+        [ForeignKey("Business")]
+        public Business Business { get; set; }
+        public int BusinessId { get; set; }
+        [ForeignKey("Address")]
+        public int AddressId { get; set; }
+        public Address Address { get; set; }
+
+        public BusinessAddress(Business business, Address address) 
+        {
+            BusinessId = business.Id; 
+            AddressId = address.Id;
+            Business = business;
+            Address = address;  
+        }
+    }
+}
