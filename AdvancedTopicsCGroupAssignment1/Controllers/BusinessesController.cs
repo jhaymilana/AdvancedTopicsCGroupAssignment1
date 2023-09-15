d638fa86b3a2dab4252338b08650292609648d0e
UDAY-2997 <udaythakur.shely@gmail.com>
2023-09-15 13:16:15 -0500

    Business Controller added

---
+++ AdvancedTopicsCGroupAssignment1/Controllers/BusinessesController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AdvancedTopicsCGroupAssignment1.Data;
+using AdvancedTopicsCGroupAssignment1.Models;
+
+namespace AdvancedTopicsCGroupAssignment1.Controllers
+{
+    public class BusinessesController : Controller
+    {
+        private readonly GroupAssignmentDbContext _context;
+
+        public BusinessesController(GroupAssignmentDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Businesses
+        public async Task<IActionResult> Index()
+        {
+              return _context.Businesss != null ? 
+                          View(await _context.Businesss.ToListAsync()) :
+                          Problem("Entity set 'GroupAssignmentDbContext.Businesss'  is null.");
+        }
+
+        // GET: Businesses/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Businesss == null)
+            {
+                return NotFound();
+            }
+
+            var business = await _context.Businesss
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (business == null)
+            {
+                return NotFound();
+            }
+
+            return View(business);
+        }
+
+        // GET: Businesses/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Businesses/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,PhoneNumber,EmailAddress")] Business business)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(business);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(business);
+        }
+
+        // GET: Businesses/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Businesss == null)
+            {
+                return NotFound();
+            }
+
+            var business = await _context.Businesss.FindAsync(id);
+            if (business == null)
+            {
+                return NotFound();
+            }
+            return View(business);
+        }
+
+        // POST: Businesses/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,PhoneNumber,EmailAddress")] Business business)
+        {
+            if (id != business.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(business);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BusinessExists(business.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(business);
+        }
+
+        // GET: Businesses/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Businesss == null)
+            {
+                return NotFound();
+            }
+
+            var business = await _context.Businesss
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (business == null)
+            {
+                return NotFound();
+            }
+
+            return View(business);
+        }
+
+        // POST: Businesses/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Businesss == null)
+            {
+                return Problem("Entity set 'GroupAssignmentDbContext.Businesss'  is null.");
+            }
+            var business = await _context.Businesss.FindAsync(id);
+            if (business != null)
+            {
+                _context.Businesss.Remove(business);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BusinessExists(int id)
+        {
+          return (_context.Businesss?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
